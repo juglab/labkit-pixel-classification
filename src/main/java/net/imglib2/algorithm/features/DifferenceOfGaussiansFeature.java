f8033a509c84e5bbae76b03a592c9b02bd8a942b
Matthias Arzt <arzt@mpi-cbg.de>
2017-07-19 15:45:34 +0200

    Migrate DifferenceOfGaussiansFeature to FeatureOp

--- src/main/java/net/imglib2/algorithm/features/DifferenceOfGaussiansFeature.java
+++ src/main/java/net/imglib2/algorithm/features/DifferenceOfGaussiansFeature.java
@@ -1,15 +1,8 @@
 package net.imglib2.algorithm.features;
 
-import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.gauss3.Gauss3;
-import net.imglib2.exception.IncompatibleTypeException;
-import net.imglib2.img.Img;
-import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.view.Views;
+import net.imglib2.algorithm.features.ops.SingleDifferenceOfGaussiansFeature;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -33,43 +26,5 @@ public class DifferenceOfGaussiansFeature {
 			for (double sigma2 = minimumSigma; sigma2 < sigma1; sigma2 *= 2)
 				features.add(new SingleDifferenceOfGaussiansFeature(sigma1, sigma2));
 		return features;
-	}
-
-	private static class SingleDifferenceOfGaussiansFeature implements Feature {
-
-		private final double sigma1;
-
-		private final double sigma2;
-
-		private SingleDifferenceOfGaussiansFeature(double sigma1, double sigma2) {
-			this.sigma1 = sigma1;
-			this.sigma2 = sigma2;
-		}
-
-		@Override
-		public int count() {
-			return 1;
-		}
-
-		@Override
-		public void apply(RandomAccessible<FloatType> in, List<RandomAccessibleInterval<FloatType>> out) {
-			dog(in, out.get(0));
-		}
-
-		@Override
-		public List<String> attributeLabels() {
-			return Collections.singletonList("Difference_of_gaussians_" + sigma1 + "_" + sigma2);
-		}
-
-		private void dog(RandomAccessible<FloatType> in, RandomAccessibleInterval<FloatType> out) {
-			try {
-				Img<FloatType> tmp = RevampUtils.ops().create().img(out);
-				Gauss3.gauss(sigma1 * 0.4, in, tmp);
-				Gauss3.gauss(sigma2 * 0.4, in, out);
-				Views.interval(Views.pair(tmp, out), out).forEach(p -> p.getB().sub(p.getA()));
-			} catch (IncompatibleTypeException e) {
-				throw new RuntimeException(e);
-			}
-		}
 	}
 }
---
+++ src/main/java/net/imglib2/algorithm/features/ops/SingleDifferenceOfGaussiansFeature.java
@@ -0,0 +1,62 @@
+package net.imglib2.algorithm.features.ops;
+
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.algorithm.features.Feature;
+import net.imglib2.algorithm.features.RevampUtils;
+import net.imglib2.algorithm.gauss3.Gauss3;
+import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Matthias Arzt
+ */
+@Plugin(type = FeatureOp.class)
+public class SingleDifferenceOfGaussiansFeature extends AbstractFeatureOp {
+
+	@Parameter
+	private double sigma1 = 2.0;
+
+	@Parameter
+	private double sigma2 = 4.0;
+
+	public SingleDifferenceOfGaussiansFeature() {}
+
+	public SingleDifferenceOfGaussiansFeature(double sigma1, double sigma2) {
+		this.sigma1 = sigma1;
+		this.sigma2 = sigma2;
+	}
+
+	@Override
+	public int count() {
+		return 1;
+	}
+
+	@Override
+	public void apply(RandomAccessible<FloatType> in, List<RandomAccessibleInterval<FloatType>> out) {
+		dog(in, out.get(0));
+	}
+
+	@Override
+	public List<String> attributeLabels() {
+		return Collections.singletonList("Difference_of_gaussians_" + sigma1 + "_" + sigma2);
+	}
+
+	private void dog(RandomAccessible<FloatType> in, RandomAccessibleInterval<FloatType> out) {
+		try {
+			Img<FloatType> tmp = RevampUtils.ops().create().img(out);
+			Gauss3.gauss(sigma1 * 0.4, in, tmp);
+			Gauss3.gauss(sigma2 * 0.4, in, out);
+			Views.interval(Views.pair(tmp, out), out).forEach(p -> p.getB().sub(p.getA()));
+		} catch (IncompatibleTypeException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
