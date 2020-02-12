bde04b793aedf7dd67eae80b468830819660f164
Matthias Arzt <arzt@mpi-cbg.de>
2020-02-12 16:45:02 +0100

    SingleLipschitzFeature: support anisotropic images

---
+++ src/main/java/net/imglib2/trainable_segmention/pixel_feature/filter/lipschitz/ConeMorphology.java
@@ -0,0 +1,96 @@
+
+package net.imglib2.trainable_segmention.pixel_feature.filter.lipschitz;
+
+import net.imglib2.Localizable;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.algorithm.neighborhood.Neighborhood;
+import net.imglib2.algorithm.neighborhood.RectangleShape;
+import net.imglib2.algorithm.neighborhood.Shape;
+import net.imglib2.trainable_segmention.RevampUtils;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.Localizables;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+import preview.net.imglib2.loops.LoopBuilder;
+
+import java.util.function.BiConsumer;
+import java.util.stream.LongStream;
+
+class ConeMorphology {
+
+	static <T extends RealType<T>> void performConeOperation(Operation operation,
+		RandomAccessibleInterval<T> inOut, double[] slope)
+	{
+		for (Localizable location : neighborhood(inOut.numDimensions())) {
+			oneRun(slope, Localizables.asLongArray(location), inOut, operation);
+		}
+	}
+
+	private static Iterable<Localizable> neighborhood(int n) {
+		Shape shape = new RectangleShape(1, true);
+		RandomAccess<Neighborhood<Localizable>> ra = shape.neighborhoodsRandomAccessible(Localizables
+			.randomAccessible(n)).randomAccess();
+		ra.setPosition(RevampUtils.nCopies(ra.numDimensions(), 0L));
+		return ra.get();
+	}
+
+	private static <T extends RealType<T>> void oneRun(double[] slope, final long[] translation,
+		final RandomAccessibleInterval<T> image, Operation operation)
+	{
+		double combinedSlope = calculateCombinedSlope(slope, translation);
+		RandomAccessibleInterval<T> centers = cutBorders(translation, image);
+		RandomAccessibleInterval<T> neighbors = cutBorders(negate(translation), image);
+		LoopBuilder.setImages(invertDirections(centers, translation), invertDirections(neighbors,
+			translation))
+			.flatIterationOrder()
+			.forEachPixel(getAction(operation, combinedSlope));
+	}
+
+	private static <T extends RealType<T>> IntervalView<T> cutBorders(long[] translation,
+		RandomAccessibleInterval<T> image)
+	{
+		return Views.interval(image, Intervals.intersect(image, Intervals.translate(image,
+			translation)));
+	}
+
+	private static long[] negate(long[] values) {
+		return LongStream.of(values).map(x -> -x).toArray();
+	}
+
+	private static <T extends RealType<T>> BiConsumer<T, T> getAction(Operation operation,
+		double combinedSlope)
+	{
+		switch (operation) {
+			case DILATION:
+				return (center, neighbor) -> center.setReal(Math.max(center.getRealDouble(), neighbor
+					.getRealDouble() - combinedSlope));
+			case EROSION:
+				return (center, neighbor) -> center.setReal(Math.min(center.getRealDouble(), neighbor
+					.getRealDouble() + combinedSlope));
+		}
+		throw new AssertionError();
+	}
+
+	private static <T> RandomAccessibleInterval<T> invertDirections(RandomAccessibleInterval<T> pair,
+		long[] translation)
+	{
+		for (int i = pair.numDimensions() - 1; i >= 0; i--)
+			if (translation[i] < 0)
+				return Views.invertAxis(pair, i);
+		return pair;
+	}
+
+	private static double calculateCombinedSlope(double[] slope, long[] translation) {
+		double combinedSlope = 0;
+		for (int i = 0; i < slope.length; i++)
+			combinedSlope += Math.pow(translation[i] * slope[i], 2);
+		combinedSlope = Math.sqrt(combinedSlope);
+		return combinedSlope;
+	}
+
+	enum Operation {
+			DILATION, EROSION
+	}
+}
---
+++ src/test/java/net/imglib2/trainable_segmention/pixel_feature/filter/lipschitz/SingleLipschitzFeatureTest.java
@@ -0,0 +1,80 @@
+
+package net.imglib2.trainable_segmention.pixel_feature.filter.lipschitz;
+
+import net.imagej.ops.OpService;
+import net.imglib2.Cursor;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.trainable_segmention.ToString;
+import net.imglib2.trainable_segmention.Utils;
+import net.imglib2.trainable_segmention.pixel_feature.filter.FeatureOp;
+import net.imglib2.trainable_segmention.pixel_feature.filter.SingleFeatures;
+import net.imglib2.trainable_segmention.pixel_feature.settings.GlobalSettings;
+import net.imglib2.type.numeric.real.FloatType;
+import org.junit.Test;
+import org.scijava.Context;
+import preview.net.imglib2.loops.LoopBuilder;
+
+import java.util.Collections;
+import java.util.function.DoubleBinaryOperator;
+
+public class SingleLipschitzFeatureTest {
+
+	@Test
+	public void test() {
+		Img<FloatType> image = dirac();
+		FeatureOp feature = SingleFeatures.lipschitz(0.1, 0).newInstance(new Context().service(
+			OpService.class), GlobalSettings.default3d().build());
+		Img<FloatType> result = ArrayImgs.floats(5, 5);
+		feature.apply(image, Collections.singletonList(result));
+		LoopBuilder.setImages(result).forEachPixel(x -> x.sub(new FloatType(255)));
+		Img<FloatType> expected = createImage((x, y) -> (x == 2) && (y == 2) ? 0 : -1 + 0.1 * Math.sqrt(
+			Math.pow(x - 2, 2) + Math.pow(y - 2, 2)));
+		System.out.println(ToString.toString(result));
+		Utils.assertImagesEqual(35, expected, result);
+	}
+
+	@Test
+	public void testOpening() {
+		Img<FloatType> image = dirac();
+		Img<FloatType> expected = createImage((x, y) -> 1 - Math.sqrt(Math.pow(0.1 * (x - 2), 2) + Math
+			.pow(0.2 * (y - 2), 2)));
+		ConeMorphology.performConeOperation(ConeMorphology.Operation.DILATION, image, new double[] {
+			0.1, 0.2 });
+		Utils.assertImagesEqual(35, expected, image);
+	}
+
+	@Test
+	public void testClosing() {
+		Img<FloatType> image = negate(dirac());
+		Img<FloatType> expected = createImage((x, y) -> -1 + Math.sqrt(Math.pow(0.1 * (x - 2), 2) + Math
+			.pow(0.2 * (y - 2), 2)));
+		ConeMorphology.performConeOperation(ConeMorphology.Operation.EROSION, image, new double[] { 0.1,
+			0.2 });
+		Utils.assertImagesEqual(35, expected, image);
+	}
+
+	// -- Helper methods --
+
+	private static Img<FloatType> dirac() {
+		return createImage((x, y) -> (x == 2) && (y == 2) ? 1 : 0);
+	}
+
+	private static Img<FloatType> createImage(DoubleBinaryOperator function) {
+		Img<FloatType> expected = ArrayImgs.floats(5, 5);
+		Cursor<FloatType> cursor = expected.cursor();
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			double x = cursor.getDoublePosition(0);
+			double y = cursor.getDoublePosition(1);
+			cursor.get().setReal(function.applyAsDouble(x, y));
+		}
+		return expected;
+	}
+
+	private Img<FloatType> negate(Img<FloatType> dirac) {
+		dirac.forEach(x -> x.mul(-1));
+		return dirac;
+	}
+
+}
