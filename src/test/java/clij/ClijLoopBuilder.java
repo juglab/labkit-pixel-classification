49356f8b4f1262e1f35ffe2e3e931f0bf44ce361
Matthias Arzt <arzt@mpi-cbg.de>
2020-02-28 23:10:32 +0100

    Add a CLIJ loop builder

---
+++ src/test/java/clij/ClijLoopBuilder.java
@@ -0,0 +1,76 @@
+package clij;
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij2.CLIJ2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * {@link ClijLoopBuilder} provides a simple way to execute pixel wise operations on images using CLIJ.
+ */
+public class ClijLoopBuilder {
+
+	private static final List<String> KERNEL_PARAMETER_NAMES = Arrays.asList("a", "b", "c");
+
+	private final CLIJ2 clij;
+
+	private final List<String> keys = new ArrayList<>();
+
+	private final List<ClearCLBuffer> images = new ArrayList<>();
+
+	private ClijLoopBuilder(CLIJ2 clij) {
+		this.clij = clij;
+	}
+
+	public static ClijLoopBuilder clij(CLIJ2 clij) {
+		return new ClijLoopBuilder(clij);
+	}
+
+	public ClijLoopBuilder setImage(String key, ClearCLBuffer image) {
+		keys.add(key);
+		images.add(image);
+		return this;
+	}
+
+	public void forEachPixel(String operation) {
+		int n = images.size();
+		if(n > KERNEL_PARAMETER_NAMES.size())
+			throw new IllegalArgumentException("Two many images.");
+		long[] dims = checkDimensions(keys, images);
+		HashMap<String, Object> parameters = new HashMap<>();
+		HashMap<String, Object> defines = new HashMap<>();
+		for (int i = 0; i < n; i++)
+			parameters.put(KERNEL_PARAMETER_NAMES.get(i), images.get(i));
+		defines.put("OPERATION_" + n + "(" + commaSeparated(keys) + ")", operation);
+		clij.execute(ClijLoopBuilderTest.class, "binary_operation.cl", "operation_" + n,
+				dims, dims, parameters, defines);
+	}
+
+	private static StringJoiner commaSeparated(List<String> keys) {
+		StringJoiner p = new StringJoiner(",");
+		for (String key : keys)
+			p.add(key);
+		return p;
+	}
+
+	private long[] checkDimensions(List<String> keys, List<ClearCLBuffer> images) {
+		long[] dims = images.get(0).getDimensions();
+		for(ClearCLBuffer image : images) {
+			if(!Arrays.equals(dims, image.getDimensions()))
+				wrongDimensionsError(keys, images);
+		}
+		return dims;
+	}
+
+	private void wrongDimensionsError(List<String> keys, List<ClearCLBuffer> images) {
+		StringJoiner joiner = new StringJoiner(" ");
+		for (int i = 0; i < keys.size(); i++) {
+			joiner.add("size(" + keys.get(i) + ")=" + Arrays.toString(images.get(i).getDimensions()) );
+		}
+		throw new IllegalArgumentException("Error the sizes of the input images don't match: " + joiner);
+	}
+}
--- src/test/java/clij/ClijLoopBuilderTest.java
+++ src/test/java/clij/ClijLoopBuilderTest.java
@@ -1,6 +1,7 @@
 package clij;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.haesleinhuepf.clij2.CLIJ2;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.array.ArrayImgs;
@@ -9,32 +10,83 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import org.junit.Test;
 
-import java.util.HashMap;
-
+/**
+ * Tests {@link ClijLoopBuilder}.
+ */
 public class ClijLoopBuilderTest {
 
 	private final CLIJ2 clij = CLIJ2.getInstance();
 
 	@Test
-	public void test() {
-		ClearCLBuffer a = clij.push(ArrayImgs.floats(new float[]{1, 2, 3, 4}, 2, 2));
-		ClearCLBuffer b = clij.push(ArrayImgs.floats(new float[]{5, 6, 7, 8}, 2, 2));
-		ClearCLBuffer c = clij.create(a);
-		binaryOp(a, b, c, "c = a + b");
-		RandomAccessibleInterval<? extends RealType<?>> result = clij.pullRAI(c);
-		RandomAccessibleInterval<FloatType> expected = ArrayImgs.floats(new float[]{6, 8, 10, 12}, 2, 2);
-		ImgLib2Assert.assertImageEqualsRealType(result, expected, 0.0);
+	public void testAdd() {
+		long[] dims = {2, 2};
+		ClearCLBuffer a = clij.push(ArrayImgs.floats(new float[]{1, 2, 3, 4}, dims));
+		ClearCLBuffer b = clij.push(ArrayImgs.floats(new float[]{5, 6, 7, 8}, dims));
+		ClearCLBuffer c = clij.create(dims);
+		add(a, b, c);
+		RandomAccessibleInterval<RealType<?>> result = clij.pullRAI(c);
+		RandomAccessibleInterval<FloatType> expected = ArrayImgs.floats(new float[]{6, 8, 10, 12}, dims);
+		ImgLib2Assert.assertImageEqualsRealType(expected, result, 0.0);
 	}
 
-	private void binaryOp(ClearCLBuffer a, ClearCLBuffer b, ClearCLBuffer c, String s) {
-		HashMap<String, Object> paramters = new HashMap<>();
-		HashMap<String, Object> defines = new HashMap<>();
-		long[] dims = a.getDimensions();
-		paramters.put("a", a);
-		paramters.put("b", b);
-		paramters.put("c", c);
-		defines.put("OPERATION(a,b,c)", s);
-		clij.execute(ClijLoopBuilderTest.class, "binary_operation.cl", "binary_operation",
-				dims, dims, paramters, defines);
+	private void add(ClearCLBuffer a, ClearCLBuffer b, ClearCLBuffer dst) {
+		ClijLoopBuilder.clij(clij)
+				.setImage("a", a)
+				.setImage("b", b)
+				.setImage("c", dst)
+				.forEachPixel("c = a + b");
+	}
+
+	@Test
+	public void testSingleImageOperation() {
+		long[] dims = {2, 2};
+		ClearCLBuffer c = clij.create(dims, NativeTypeEnum.Float);
+		ClijLoopBuilder.clij(clij)
+				.setImage("output", c)
+				.forEachPixel("output = 2.0");
+		RandomAccessibleInterval<RealType<?>> result = clij.pullRAI(c);
+		RandomAccessibleInterval<FloatType> expected = ArrayImgs.floats(new float[]{2, 2, 2, 2}, 2, 2);
+		ImgLib2Assert.assertImageEqualsRealType(expected, result, 0.0);
+	}
+
+	@Test
+	public void testTwoImageOperation() {
+		long[] dims = {2, 2};
+		ClearCLBuffer c = clij.create(dims, NativeTypeEnum.Byte);
+		ClearCLBuffer a = clij.push(ArrayImgs.floats(new float[]{1, 2, 3, 4}, dims));
+		ClijLoopBuilder.clij(clij)
+				.setImage("in", a)
+				.setImage("out", c)
+				.forEachPixel("out = 2.0 * in");
+		RandomAccessibleInterval<RealType<?>> result = clij.pullRAI(c);
+		RandomAccessibleInterval<FloatType> expected = ArrayImgs.floats(new float[]{2, 4, 6, 8}, dims);
+		ImgLib2Assert.assertImageEqualsRealType(expected, result, 0.0);
+	}
+
+	@Test
+	public void testMultipleOutputs() {
+		long[] dims = {2, 2};
+		ClearCLBuffer a = clij.push(ArrayImgs.floats(new float[]{1, 2, 3, 4}, dims));
+		ClearCLBuffer b = clij.create(dims);
+		ClearCLBuffer c = clij.create(dims);
+		ClijLoopBuilder.clij(clij)
+				.setImage("a", a)
+				.setImage("b", b)
+				.setImage("c", c)
+				.forEachPixel("b = 2 * a; c = a + b");
+		RandomAccessibleInterval<RealType<?>> resultB = clij.pullRAI(b);
+		RandomAccessibleInterval<RealType<?>> resultC = clij.pullRAI(c);
+		ImgLib2Assert.assertImageEqualsRealType(ArrayImgs.floats(new float[]{2, 4, 6, 8}, dims), resultB, 0.0);
+		ImgLib2Assert.assertImageEqualsRealType(ArrayImgs.floats(new float[]{3, 6, 9, 12}, dims), resultC, 0.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testMismatchingDimensions() {
+		ClearCLBuffer c = clij.create(new long[]{10, 10});
+		ClearCLBuffer b = clij.create(new long[]{10, 11});
+		ClijLoopBuilder.clij(clij)
+				.setImage("c", c)
+				.setImage("b", b)
+				.forEachPixel("b = c");
 	}
 }
