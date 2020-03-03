9108dc71775844978e88edcc191036adccae6f32
Matthias Arzt <arzt@mpi-cbg.de>
2020-03-03 13:40:55 +0100

    Add method to copy sub intervals of a ClearCLBuffer

---
+++ src/main/java/net/imglib2/trainable_segmention/clij_random_forest/CLIJView.java
@@ -0,0 +1,40 @@
+package net.imglib2.trainable_segmention.clij_random_forest;
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
+import net.imglib2.util.Intervals;
+
+import java.util.stream.LongStream;
+
+public class CLIJView {
+
+	private final ClearCLBuffer buffer;
+	private final Interval interval;
+
+	public CLIJView(ClearCLBuffer buffer, Interval interval) {
+		this.buffer = buffer;
+		this.interval = interval;
+	}
+
+	public ClearCLBuffer buffer() {
+		return buffer;
+	}
+
+	public Interval interval() {
+		return interval;
+	}
+
+	public static CLIJView shrink(ClearCLBuffer buffer, long border) {
+		return new CLIJView(buffer, Intervals.expand(new FinalInterval(buffer.getDimensions()), -border) );
+	}
+
+	public static CLIJView shrink(ClearCLBuffer buffer, long[] border) {
+		long[] negativeBorder = LongStream.of(border).map(x -> -x).toArray();
+		return new CLIJView(buffer, Intervals.expand(new FinalInterval(buffer.getDimensions()), negativeBorder));
+	}
+
+	public static CLIJView wrap(ClearCLBuffer buffer) {
+		return new CLIJView(buffer, new FinalInterval(buffer.getDimensions()));
+	}
+}
