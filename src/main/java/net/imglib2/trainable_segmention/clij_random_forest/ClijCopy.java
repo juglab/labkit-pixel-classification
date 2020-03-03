07542da807177414c9efca096d8afebeb32178ef
Matthias Arzt <arzt@mpi-cbg.de>
2020-03-03 13:40:55 +0100

    Add method to copy sub intervals of a ClearCLBuffer

--- src/main/java/net/imglib2/trainable_segmention/clij_random_forest/ClijCopy.java
+++ src/main/java/net/imglib2/trainable_segmention/clij_random_forest/ClijCopy.java
@@ -3,6 +3,8 @@ package net.imglib2.trainable_segmention.clij_random_forest;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij2.CLIJ2;
+import net.imglib2.Interval;
+import net.imglib2.util.Intervals;
 
 import java.util.HashMap;
 
@@ -19,5 +21,28 @@ public class ClijCopy {
 		parameters.put("step", step);
 		clij.execute(ClijCopy.class, "copy_3d_stack.cl", "copy_3d_stack", globalSizes, globalSizes,
 			parameters);
+	}
+
+	public static void copy(CLIJ2 clij, CLIJView src, CLIJView dst) {
+		HashMap<String, Object> parameters = new HashMap<>();
+		parameters.put("src_offset_x", min(src, 0));
+		parameters.put("src_offset_y", min(src, 1));
+		parameters.put("src_offset_z", min(src, 2));
+		parameters.put("dst_offset_x", min(dst, 0));
+		parameters.put("dst_offset_y", min(dst, 1));
+		parameters.put("dst_offset_z", min(dst, 2));
+		parameters.put("src", src.buffer());
+		parameters.put("dst", dst.buffer());
+		long[] globalSizes = Intervals.dimensionsAsLongArray(src.interval());
+		if (!Intervals.equalDimensions(src.interval(), dst.interval()))
+			throw new IllegalArgumentException();
+		clij.execute(ClijCopy.class, "copy_with_offset.cl", "copy_with_offset", globalSizes,
+			globalSizes,
+			parameters);
+	}
+
+	private static int min(CLIJView view, int d) {
+		Interval interval = view.interval();
+		return (int) (d < interval.numDimensions() ? interval.min(d) : 0);
 	}
 }
---
+++ src/test/java/net/imglib2/trainable_segmention/clij_random_forest/ClijCopyTest.java
@@ -0,0 +1,48 @@
+
+package net.imglib2.trainable_segmention.clij_random_forest;
+
+import net.haesleinhuepf.clij.CLIJ;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
+import net.haesleinhuepf.clij2.CLIJ2;
+import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.test.ImgLib2Assert;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests {@link ClijCopy}
+ */
+public class ClijCopyTest {
+
+	private CLIJ2 clij;
+
+	@Before
+	public void before() {
+		clij = CLIJ2.getInstance();
+	}
+
+	@After
+	public void after() {
+		clij.close();
+	}
+
+	@Test
+	public void testCopy() {
+		ClearCLBuffer source = clij.push(ArrayImgs.floats(new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 3,
+			3));
+		ClearCLBuffer destination = clij.create(new long[] { 3, 3 }, NativeTypeEnum.Float);
+		CLIJView sourceView = new CLIJView(source, Intervals.createMinSize(0, 1, 2, 2));
+		CLIJView destinationView = new CLIJView(destination, Intervals.createMinSize(1, 0, 2, 2));
+		ClijCopy.copy(clij, sourceView, destinationView);
+		RandomAccessibleInterval<FloatType> result = clij.pullRAI(destination);
+		RandomAccessibleInterval<FloatType> expected = ArrayImgs.floats(new float[] { 0, 4, 5, 0, 7, 8,
+			0, 0, 0 }, 3, 3);
+		ImgLib2Assert.assertImageEquals(expected, result);
+	}
+}
