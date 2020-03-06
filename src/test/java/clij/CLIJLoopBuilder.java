d48b72697b365b20476c671a3294838c42fa7a7a
Matthias Arzt <arzt@mpi-cbg.de>
2020-03-06 23:31:08 +0100

    CLIJLoopBuilder with arbitrary number of paramters

--- src/test/java/clij/CLIJLoopBuilder.java
+++ src/test/java/clij/CLIJLoopBuilder.java
@@ -14,7 +14,7 @@ import java.util.StringJoiner;
  */
 public class CLIJLoopBuilder {
 
-	private static final List<String> KERNEL_PARAMETER_NAMES = Arrays.asList("a", "b", "c");
+	private static final List<String> KERNEL_PARAMETER_NAMES = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
 
 	private final CLIJ2 clij;
 
@@ -43,10 +43,18 @@ public class CLIJLoopBuilder {
 		long[] dims = checkDimensions(keys, images);
 		HashMap<String, Object> parameters = new HashMap<>();
 		HashMap<String, Object> defines = new HashMap<>();
-		for (int i = 0; i < n; i++)
-			parameters.put(KERNEL_PARAMETER_NAMES.get(i), images.get(i));
-		defines.put("OPERATION_" + n + "(" + commaSeparated(keys) + ")", operation);
-		clij.execute(CLIJLoopBuilderTest.class, "binary_operation.cl", "operation_" + n,
+		StringJoiner parameter_define = new StringJoiner(", ");
+		StringJoiner operation_define = new StringJoiner(", ", "OPERATION_FUNCTION(", ")");
+		for (int i = 0; i < n; i++) {
+			String key = KERNEL_PARAMETER_NAMES.get(i);
+			parameter_define.add("IMAGE_" + key + "_TYPE " + key);
+			operation_define.add("PIXEL(" + key + ")");
+			parameters.put(key, images.get(i));
+		}
+		defines.put("PARAMETER", parameter_define.toString());
+		defines.put("OPERATION", operation_define.toString());
+		defines.put("OPERATION_FUNCTION(" + commaSeparated(keys) + ")", operation);
+		clij.execute(CLIJLoopBuilderTest.class, "binary_operation.cl", "operation",
 				dims, dims, parameters, defines);
 	}
 
