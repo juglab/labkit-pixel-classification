57f57c551bd6f4cb77dfbaac40fee90ba55f3e19
Matthias Arzt <arzt@mpi-cbg.de>
2020-03-09 14:51:27 +0100

    Test separable convolution in y direction

--- src/test/java/clij/Gauss.java
+++ src/test/java/clij/Gauss.java
@@ -4,34 +4,74 @@ package clij;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij.clearcl.util.CLKernelExecutor;
 import net.haesleinhuepf.clij2.CLIJ2;
+import net.imglib2.Interval;
+import net.imglib2.trainable_segmention.clij_random_forest.CLIJView;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Gauss {
 
 	private static CLKernelExecutor executor;
 
-	public static void convolve(CLIJ2 clij, ClearCLBuffer inputBuffer, ClearCLBuffer kernelBuffer,
-		ClearCLBuffer outputBuffer)
+	public static void convolve(CLIJ2 clij, ClearCLBuffer input, ClearCLBuffer kernel,
+		ClearCLBuffer output)
+	{
+		convolve(clij, CLIJView.wrap(input), kernel, CLIJView.wrap(output), 0);
+	}
+
+	public static void convolve(CLIJ2 clij, CLIJView input, ClearCLBuffer kernel, CLIJView output,
+		int d)
 	{
 		HashMap<String, Object> parameters = new HashMap<>();
-		parameters.put("input", inputBuffer);
-		parameters.put("kernelValues", kernelBuffer);
-		parameters.put("output", outputBuffer);
+		parameters.put("input", input.buffer());
+		parameters.put("kernelValues", kernel);
+		parameters.put("output", output.buffer());
 		HashMap<String, Object> defines = new HashMap<>();
-		long[] localSizes = { outputBuffer.getWidth(), 1, 1 };
-		defines.put("KERNEL_LENGTH", kernelBuffer.getWidth());
+		long[] localSizes = new long[3];
+		Arrays.fill(localSizes, 1);
+		localSizes[0] = output.interval().dimension(d);
+		defines.put("KERNEL_LENGTH", kernel.getWidth());
 		defines.put("BLOCK_SIZE", localSizes[0]);
-		defines.put("INPUT_X_SKIP", 1L);
-		defines.put("INPUT_Y_SKIP", inputBuffer.getWidth());
-		defines.put("INPUT_Z_SKIP", inputBuffer.getWidth() * inputBuffer.getHeight());
-		defines.put("INPUT_OFFSET", 0L);
-		defines.put("OUTPUT_X_SKIP", 1L);
-		defines.put("OUTPUT_Y_SKIP", outputBuffer.getWidth());
-		defines.put("OUTPUT_Z_SKIP", outputBuffer.getWidth() * outputBuffer.getHeight());
-		defines.put("OUTPUT_OFFSET", 0L);
+		setSkips(defines, "INPUT", input, d);
+		setSkips(defines, "OUTPUT", output, d);
+		long[] globalSizes = getDimensions(output.interval());
+		ArrayUtils.swap(globalSizes, 0, d);
+		clij.execute(Gauss.class, "gauss.cl", "convolve1d", globalSizes,
+			globalSizes, localSizes, parameters, defines);
+	}
 
-		clij.execute(Gauss.class, "gauss.cl", "convolve1d", outputBuffer.getDimensions(),
-			outputBuffer.getDimensions(), localSizes, parameters, defines);
+	private static void setSkips(HashMap<String, Object> defines, String prefix, CLIJView view,
+		int d)
+	{
+		ClearCLBuffer buffer = view.buffer();
+		Interval interval = view.interval();
+		long[] skip = { 1, buffer.getWidth(), buffer.getWidth() * buffer.getHeight() };
+		defines.put(prefix + "_OFFSET", getOffset(interval, skip));
+		ArrayUtils.swap(skip, 0, d);
+		defines.put(prefix + "_X_SKIP", skip[0]);
+		defines.put(prefix + "_Y_SKIP", skip[1]);
+		defines.put(prefix + "_Z_SKIP", skip[2]);
+	}
+
+	private static long getOffset(Interval interval, long[] skip) {
+		long result = 0;
+		for (int i = 0; i < interval.numDimensions(); i++) {
+			result += skip[i] * interval.min(i);
+		}
+		return result;
+	}
+
+	private static long[] getDimensions(Interval interval) {
+		return new long[] {
+			getDimension(interval, 0),
+			getDimension(interval, 1),
+			getDimension(interval, 2),
+		};
+	}
+
+	private static long getDimension(Interval interval, int d) {
+		return d < interval.numDimensions() ? interval.dimension(d) : 1;
 	}
 }
--- src/test/java/clij/GaussTest.java
+++ src/test/java/clij/GaussTest.java
@@ -3,34 +3,83 @@ package clij;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij2.CLIJ2;
+import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.FloatArray;
 import net.imglib2.test.ImgLib2Assert;
+import net.imglib2.trainable_segmention.ToString;
+import net.imglib2.trainable_segmention.clij_random_forest.CLIJView;
 import net.imglib2.type.numeric.real.FloatType;
+import org.junit.Ignore;
 import org.junit.Test;
-
-import java.util.HashMap;
 
 public class GaussTest {
 
 	CLIJ2 clij = CLIJ2.getInstance();
 
-	float[] kernel = { -0.5f, 0, 0.5f };
-
 	@Test
 	public void test() {
-		Img<FloatType> input = ArrayImgs.floats(new float[] { 0, 0, 1, 0, 0 }, 5, 1);
-		Img<FloatType> kernelImg = ArrayImgs.floats(kernel, kernel.length, 1);
-		Img<FloatType> expected = ArrayImgs.floats(new float[] { 0.5f, 0, -0.5f }, 3, 1);
 		try (
-			ClearCLBuffer inputBuffer = clij.push(input);
-			ClearCLBuffer kernelBuffer = clij.push(kernelImg);
-			ClearCLBuffer outputBuffer = clij.create(new long[] { 3, 1 });)
+			ClearCLBuffer input = clij.push(img1D(0, 0, 1, 0, 0));
+			ClearCLBuffer kernel = clij.push(img1D(-0.5f, 0, 0.5f));
+			ClearCLBuffer output = clij.create(new long[] { 3, 1 });)
 		{
-			Gauss.convolve(clij, inputBuffer, kernelBuffer, outputBuffer);
-			RandomAccessibleInterval output = clij.pullRAI(outputBuffer);
-			ImgLib2Assert.assertImageEqualsRealType(expected, output, 0);
+			Gauss.convolve(clij, input, kernel, output);
+			ImgLib2Assert.assertImageEqualsRealType(img1D(0.5f, 0, -0.5f), clij.pullRAI(output), 0);
 		}
+	}
+
+	@Test
+	public void testBorder() {
+		try (
+			ClearCLBuffer input = clij.push(img1D(2, 0, 0, 0, 3));
+			ClearCLBuffer kernel = clij.push(img1D(-0.5f, 0, 0.5f));
+			ClearCLBuffer output = clij.create(new long[] { 3, 1 });)
+		{
+			Gauss.convolve(clij, input, kernel, output);
+			ImgLib2Assert.assertImageEqualsRealType(img1D(-1, 0, 1.5f), clij.pullRAI(output), 0);
+		}
+	}
+
+	@Ignore("FIXME")
+	@Test
+	public void testLongLine() {
+		int length = 10000;
+		try (
+			ClearCLBuffer input = clij.push(img1D(new float[length + 2]));
+			ClearCLBuffer kernel = clij.push(img1D(-0.5f, 0, 0.5f));
+			ClearCLBuffer output = clij.create(new long[] { length, 1 });)
+		{
+			Gauss.convolve(clij, input, kernel, output);
+			ImgLib2Assert.assertImageEqualsRealType(img1D(new float[length]), clij.pullRAI(output), 0);
+		}
+	}
+
+	@Test
+	public void testY() {
+		try (
+			ClearCLBuffer input = clij.push(ArrayImgs.floats(new float[] {
+				0, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, -1, 0
+			}, 4, 3));
+			ClearCLBuffer kernel = clij.push(img1D(1, 2));
+			ClearCLBuffer output = clij.create(new long[] { 3, 2 });)
+		{
+			Gauss.convolve(clij, CLIJView.interval(input, FinalInterval.createMinSize(1, 0, 2, 2)),
+				kernel, CLIJView.wrap(output), 1);
+			RandomAccessibleInterval actual = clij.pullRAI(output);
+			System.out.println(ToString.toString(actual));
+			ImgLib2Assert.assertImageEqualsRealType(ArrayImgs.floats(new float[] {
+				2, 0, 0,
+				1, -2, 0
+			}, 3, 2), actual, 0);
+		}
+	}
+
+	private ArrayImg<FloatType, FloatArray> img1D(float... array) {
+		return ArrayImgs.floats(array, array.length, 1);
 	}
 }
