320dec7d590dc84db7fc8f16c61e203e818c2a1d
Matthias Arzt <arzt@mpi-cbg.de>
2020-04-02 23:15:27 +0200

    GaussBenchmark: show potential 3x speed up

--- src/test/java/clij/GaussBenchmark.java
+++ src/test/java/clij/GaussBenchmark.java
@@ -2,19 +2,22 @@
 package clij;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.clearcl.enums.HostAccessType;
+import net.haesleinhuepf.clij.clearcl.enums.KernelAccessType;
+import net.haesleinhuepf.clij.clearcl.enums.MemAllocMode;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.haesleinhuepf.clij2.CLIJ2;
 import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.RealTypeConverters;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.test.RandomImgs;
 import net.imglib2.trainable_segmention.Utils;
-import net.imglib2.trainable_segmention.clij_random_forest.CLIJFeatureInput;
-import net.imglib2.trainable_segmention.clij_random_forest.CLIJMultiChannelImage;
 import net.imglib2.trainable_segmention.clij_random_forest.CLIJView;
 import net.imglib2.trainable_segmention.clij_random_forest.compute_cache.ComputeCache;
 import net.imglib2.trainable_segmention.clij_random_forest.compute_cache.GaussContent;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Intervals;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
@@ -27,9 +30,13 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import preview.net.imglib2.algorithm.convolution.kernel.Kernel1D;
+import preview.net.imglib2.algorithm.gauss3.Gauss3;
 
-import java.util.List;
+import java.nio.FloatBuffer;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.DoubleStream;
 
 @Fork(1)
 @Warmup(iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS)
@@ -46,6 +53,12 @@ public class GaussBenchmark {
 	private final RandomAccessible<FloatType> input = Utils.dirac(3);
 	private final ComputeCache cache = new ComputeCache(clij, input, new double[] { 1, 1, 1 });
 	private final GaussContent content = new GaussContent(cache, 8);
+	private final ClearCLBuffer kernel = gaussKernel(8);
+	private final long large = 64 + kernel.getWidth() - 1;
+	private final ClearCLBuffer inputBuffer2 = clij.push(RandomImgs.seed(1).nextImage(new FloatType(),
+		large, large, large));
+	private final ClearCLBuffer tmp1 = createNoAccessBuffer(64, large, large);
+	private final ClearCLBuffer tmp2 = createNoAccessBuffer(64, 64, large);
 
 	@Setup
 	public void setUp() {
@@ -55,6 +68,11 @@ public class GaussBenchmark {
 	@TearDown
 	public void tearDown() {
 		output.close();
+		inputBuffer.close();
+		inputBuffer2.close();
+		tmp1.close();
+		tmp2.close();
+		kernel.close();
 		clij.clear();
 	}
 
@@ -69,8 +87,55 @@ public class GaussBenchmark {
 		return clij.pullRAI(output);
 	}
 
+	@Benchmark
+	public RandomAccessibleInterval compare() {
+		CLIJKernelConvolution.convolve(clij, CLIJView.wrap(inputBuffer2), kernel, CLIJView.wrap(tmp1),
+			0);
+		CLIJKernelConvolution.convolve(clij, CLIJView.wrap(tmp1), kernel, CLIJView.wrap(tmp2), 1);
+		CLIJKernelConvolution.convolve(clij, CLIJView.wrap(tmp2), kernel, CLIJView.wrap(output), 2);
+		return clij.pullRAI(output);
+	}
+
+	@Benchmark
+	public RandomAccessibleInterval compare2() {
+		ClearCLBuffer tmp1 = clij.create(64, large, large);
+		ClearCLBuffer tmp2 = clij.create(64, 64, large);
+		ClearCLBuffer kernel1 = gaussKernel(8);
+		CLIJKernelConvolution.convolve(clij, CLIJView.wrap(inputBuffer2), kernel1, CLIJView.wrap(tmp1),
+			0);
+		ClearCLBuffer kernel2 = gaussKernel(8);
+		CLIJKernelConvolution.convolve(clij, CLIJView.wrap(tmp1), kernel2, CLIJView.wrap(tmp2), 1);
+		ClearCLBuffer kernel3 = gaussKernel(8);
+		CLIJKernelConvolution.convolve(clij, CLIJView.wrap(tmp2), kernel3, CLIJView.wrap(output), 2);
+		tmp1.close();
+		tmp2.close();
+		kernel1.close();
+		kernel2.close();
+		kernel2.close();
+		return clij.pullRAI(output);
+	}
+
 	public static void main(String... args) throws RunnerException {
 		Options options = new OptionsBuilder().include(GaussBenchmark.class.getSimpleName()).build();
 		new Runner(options).run();
 	}
+
+	private ClearCLBuffer gaussKernel(double sigma) {
+		double[] fullKernel = Kernel1D.symmetric(Gauss3.halfkernels(new double[] { sigma })[0])
+			.fullKernel();
+		float[] kernel = new float[fullKernel.length];
+		for (int i = 0; i < kernel.length; i++)
+			kernel[i] = (float) fullKernel[i];
+		ClearCLBuffer buffer = clij.getCLIJ().getClearCLContext().createBuffer(
+			MemAllocMode.AllocateHostPointer, HostAccessType.WriteOnly, KernelAccessType.ReadOnly, 1,
+			NativeTypeEnum.Float, new long[] { kernel.length });
+		buffer.readFrom(FloatBuffer.wrap(kernel), true);
+		return buffer;
+	}
+
+	private ClearCLBuffer createNoAccessBuffer(long... dims) {
+		return clij.getCLIJ().getClearCLContext().createBuffer(MemAllocMode.AllocateHostPointer,
+			HostAccessType.NoAccess, KernelAccessType.ReadWrite, 1, NativeTypeEnum.Float, dims);
+	}
+
 }
