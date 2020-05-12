0a9760d873c7d11ea74d362ec4615e733e69b45c
Matthias Arzt <arzt@mpi-cbg.de>
2020-05-12 14:37:35 +0200

    Fix javadoc

--- src/main/java/net/imglib2/trainable_segmention/gpu/random_forest/TransparentRandomTree.java
+++ src/main/java/net/imglib2/trainable_segmention/gpu/random_forest/TransparentRandomTree.java
@@ -5,7 +5,7 @@ import net.imglib2.util.Cast;
 import weka.core.Instance;
 
 /**
- * Wraps around a {@link hr.irb.fastRandomForest.FastRandomTree} and exposes all
+ * Wraps around a hr.irb.fastRandomForest.FastRandomTree and exposes all
  * the parameters (threshold, children and classification probabilities).
  * <p>
  * Java reflection is used to extract the parameter from the weka fast random
