cc3f29c3de937ab1b5210b75776117c0a647584f
Matthias Arzt <arzt@mpi-cbg.de>
2021-02-08 16:20:07 +0100

    Evaluate alternative to Views.collapse(Views.stack())
    Conclusion:
    
    * Views.stack(...) is slow.
    * Alternative VectorRandomAccess is only slightly better.
    * Using arrays is best, nearly reached by using cursers.

--- src/test/java/net/imglib2/trainable_segmentation/pixel_feature/filter/hessian/EigenValueBenchmark.java
+++ src/test/java/net/imglib2/trainable_segmentation/pixel_feature/filter/hessian/EigenValueBenchmark.java
@@ -1,15 +1,17 @@
 
 package net.imglib2.trainable_segmentation.pixel_feature.filter.hessian;
 
+import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.linalg.eigen.EigenValues;
-import net.imglib2.algorithm.linalg.eigen.EigenValuesSymmetric;
 import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.test.RandomImgs;
 import net.imglib2.trainable_segmentation.RevampUtils;
 import net.imglib2.trainable_segmentation.utils.CubicEquation;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Cast;
@@ -33,8 +35,18 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static net.imglib2.test.ImgLib2Assert.assertImageEquals;
-
+/**
+ * WIP: This benchmark compares alternatives to using a combination of
+ * LoopBuilder + Views.collapse(Views.stack(...))
+ * <p>
+ * Conclusion: LoopBuilder + collapse + stack. Is very slow. Performance is half
+ * as good as using arrays directly. Views.stack(...) seems to add the most overhead.
+ * We get good performance by using cursors directly on ArrayImgs.
+ * Medium performance can be achieved by avoiding the use of Views.stack(), but
+ * this requires the input to by a multichannel image instead of a list of images.
+ * VectorRandomAccess achieves better performance than the combination Views.stack +
+ * Views.collapse but is still not great.
+ */
 @Fork(1)
 @Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS)
@@ -44,11 +56,15 @@ public class EigenValueBenchmark {
 	private final long dims[] = { 50, 50, 50 };
 
 	private final List<RandomAccessibleInterval<DoubleType>> slices = IntStream.range(0, 6)
-		.mapToObj(i -> RandomImgs.seed(i).nextImage(new DoubleType(), dims))
+		.mapToObj(i -> RandomImgs.seed(i).randomize(image(new DoubleType())))
 		.collect(Collectors.toList());
 
+	private <T extends NativeType<T>> RandomAccessibleInterval<T> image(T type) {
+		return new ArrayImgFactory<>(type).create(dims);
+	}
+
 	private final List<RandomAccessibleInterval<FloatType >> eigenvalues = IntStream.range(0, 3)
-		.mapToObj(i -> ArrayImgs.floats(dims))
+		.mapToObj(i -> image(new FloatType()))
 		.collect(Collectors.toList());
 
 	private final RandomAccessibleInterval<DoubleType> blockInput = init();
@@ -64,18 +80,8 @@ public class EigenValueBenchmark {
 			blockOutput = ArrayImgs.floats(50, 50, 50, 3);
 
 	@Benchmark
-	public Object benchmarkImgLib2AlgorithmEigenValuesSymmetric3D() {
-		RandomAccessibleInterval<Composite<DoubleType>> matrix = RevampUtils.vectorizeStack(slices);
-		RandomAccessibleInterval<Composite<FloatType>> result = RevampUtils.vectorizeStack(
-			eigenvalues);
-		EigenValuesSymmetric<DoubleType, FloatType> calculator = EigenValues.symmetric(3);
-		LoopBuilder.setImages(matrix, result).forEachPixel(
-				(tensor, evs) -> calculator.compute(tensor, evs));
-		return eigenvalues;
-	}
-
-	@Benchmark
-	public Object benchmarkEigenValuesSymmetric3D() {
+	public Object useVectorRandomAccess() {
+		// NB: This is a little bit faster than Views.collapse + Views.stack but still has the same syntax.
 		RandomAccessibleInterval<Composite<DoubleType>> matrix = RevampUtils.vectorizeStack(slices);
 		RandomAccessibleInterval<Composite<FloatType>> result = RevampUtils.vectorizeStack(
 			eigenvalues);
@@ -86,7 +92,8 @@ public class EigenValueBenchmark {
 	}
 
 	@Benchmark
-	public Object imglib2Composite() {
+	public Object useImgLib2ViewsCollapseAndStack() {
+		// NB: this is slowest
 		RandomAccessibleInterval<Composite<DoubleType>> matrix =
 				Cast.unchecked(Views.collapse(Views.stack(slices)));
 		RandomAccessibleInterval<Composite<FloatType>> result =
@@ -97,18 +104,32 @@ public class EigenValueBenchmark {
 	}
 
 	@Benchmark
-	public Object imglib2Composite2() {
+	public Object useNoLoopBuilder() {
+		// NB: This shows that using LoopBuilder is not the problem.
 		RandomAccessibleInterval<Composite<DoubleType>> matrix =
-				Cast.unchecked(Views.collapse(blockInput));
+				Cast.unchecked(Views.collapse(Views.stack(slices)));
 		RandomAccessibleInterval<Composite<FloatType>> result =
-				Cast.unchecked(Views.collapse(blockOutput));
+				Cast.unchecked(Views.collapse(Views.stack(eigenvalues)));
+		EigenValues<DoubleType, FloatType> calculator = new EigenValuesSymmetric3D<>();
+		Cursor< Composite< DoubleType > > m = Views.flatIterable(matrix).cursor();
+		Cursor< Composite< FloatType > > e = Views.flatIterable(result).cursor();
+		while (m.hasNext()) {
+			calculator.compute(m.next(), e.next());
+		}
+		return eigenvalues;
+	}
+
+	@Benchmark
+	public Object useImgLib2ViewsCollapseOnly() {
+		RandomAccessibleInterval<Composite<DoubleType>> matrix = Cast.unchecked(Views.collapse(blockInput));
+		RandomAccessibleInterval<Composite<FloatType>> result = Cast.unchecked(Views.collapse(blockOutput));
 		EigenValues<DoubleType, FloatType> calculator = new EigenValuesSymmetric3D<>();
 		LoopBuilder.setImages(matrix, result).forEachPixel( (matrix1, evs) -> calculator.compute(matrix1, evs));
 		return eigenvalues;
 	}
 
 	@Benchmark
-	public Object arrays() {
+	public Object useArrays() {
 		double[] i0 = getArray(slices.get(0));
 		double[] i1 = getArray(slices.get(1));
 		double[] i2 = getArray(slices.get(2));
@@ -139,6 +160,33 @@ public class EigenValueBenchmark {
 		return eigenvalues;
 	}
 
+	@Benchmark
+	public Object useCursors() {
+		Cursor< DoubleType > i0 = Views.flatIterable(slices.get(0)).cursor();
+		Cursor< DoubleType > i1 = Views.flatIterable(slices.get(1)).cursor();
+		Cursor< DoubleType > i2 = Views.flatIterable(slices.get(2)).cursor();
+		Cursor< DoubleType > i3 = Views.flatIterable(slices.get(3)).cursor();
+		Cursor< DoubleType > i4 = Views.flatIterable(slices.get(4)).cursor();
+		Cursor< DoubleType > i5 = Views.flatIterable(slices.get(5)).cursor();
+		Cursor<FloatType> o0 = Views.flatIterable(eigenvalues.get(0)).cursor();
+		Cursor<FloatType> o1 = Views.flatIterable(eigenvalues.get(1)).cursor();
+		Cursor<FloatType> o2 = Views.flatIterable(eigenvalues.get(2)).cursor();
+		double[] e = new double[3];
+		while (o1.hasNext()) {
+			final double a11 = i0.next().getRealDouble();
+			final double a12 = i1.next().getRealDouble();
+			final double a13 = i2.next().getRealDouble();
+			final double a22 = i3.next().getRealDouble();
+			final double a23 = i4.next().getRealDouble();
+			final double a33 = i5.next().getRealDouble();
+			calc(e, a11, a12, a13, a22, a23, a33);
+			o0.next().setReal(e[2]);
+			o1.next().setReal(e[1]);
+			o2.next().setReal(e[0]);
+		}
+		return eigenvalues;
+	}
+
 	public <T> T getArray(RandomAccessibleInterval arrayImg) {
 		return Cast.unchecked(((ArrayDataAccess) ((ArrayImg) arrayImg).update(null)).getCurrentStorageArray());
 	}
@@ -157,10 +205,5 @@ public class EigenValueBenchmark {
 		Options options = new OptionsBuilder().include(EigenValueBenchmark.class.getSimpleName())
 			.build();
 		new Runner(options).run();
-		EigenValueBenchmark one = new EigenValueBenchmark();
-		one.arrays();
-		EigenValueBenchmark two = new EigenValueBenchmark();
-		two.benchmarkEigenValuesSymmetric3D();
-		assertImageEquals(Views.stack(two.eigenvalues), Views.stack(one.eigenvalues));
 	}
 }
